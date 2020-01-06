74374fba7118248a5958dfee93fa2523acec3462
EvgeniyBudaev <budaev.e@gmail.com>
2020-01-06 17:13:43 +0300

    add life cycles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Header from "../header";
 import RandomPlanet from "../random-planet";
-import ItemList from "../item-list";
-import PersonDetails from "../person-details";
-
+import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page";
 import "./app.css";
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
+  
+  state = {
+    showRandomPlanet: true,
+    hasError: false
+  };
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
-        </div>
+  // toggleRandomPlanet = () => {
+  //   this.setState({
+
+  //   });
+  // }
+
+  componentDidCatch() {
+    console.log("componentDidCatch()");
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+
+    return (
+      <div className="stardb-app">
+        <Header />
+        {planet}
+
+        {/* <button
+        className="toggle-planet btn btn-warning btn-lg"
+        onClick={this.toggleRandomPlanet}>
+         Toggle Random Planet
+        </button>
+   */}
+        <PeoplePage />
+
+
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
